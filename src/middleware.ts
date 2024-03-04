f328c59aead6255bc4fbec62230c0a2a32f94c75
Ive <ive_tourinho@hotmail.com>
2024-03-04 00:01:15 -0300

    feat: add ondelete cascade to schema

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,19 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getSession } from '@auth0/nextjs-auth0/edge';
+//import { jwtDecode } from "jwt-decode";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const session = await getSession(request, response);
 
-  if (session?.user) {
+  /* if (session?.idToken) {
+    const decoded = jwtDecode(session?.idToken);
+    console.log('decoded sub', decoded.sub)
+    console.log('session', session.user.email, session.user.sub)
+  } */
+
+  if (session?.user || request.nextUrl.pathname.endsWith('/')) {
     return NextResponse.next();
   }
  
